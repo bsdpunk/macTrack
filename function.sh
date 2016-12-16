bec2cbf2f808b29901ad30eb2d353e90f555317e
Dustin Carver <bsdpunk@gmail.com>
2016-12-16 04:55:24 -0600

    minor

--- function.sh
+++ function.sh
@@ -20,4 +20,6 @@
 set -o nounset                              # Treat unset variables as an error
 function snap () { ps aux | awk '{print $3,$4,$1,$2,$11}'| sort -rnk1,1 | grep -v '0.0 0.0' && echo bk && vm_stat && echo bk &&  osascript -e 'path to frontmost application as text' ; }
 
+
+#echo $(cat ~/Documents/snap.log | grep bk | wc -l) \/ 3 | bc
 #while true; do sleep 360 && echo bk >> ~/Documents/snap.log && date >> ~/Documents/snap.log && echo bk >> ~/Documents/snap.log && snap >> ~/Documents/snap.log; done
