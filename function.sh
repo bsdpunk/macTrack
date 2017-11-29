82439fc17d63b73c13d52ba8306291c330e5da00
Dusty <bsdpunk@gmail.com>
2017-11-29 13:12:30 -0600

    MadeBashDustyReadable

--- function.sh
+++ function.sh
@@ -18,11 +18,19 @@
 #===============================================================================
 
 set -o nounset                              # Treat unset variables as an error
-function snap () { ps aux | awk '{print $3,$4,$1,$2,$11}'| sort -rnk1,1 | grep -v '0.0 0.0' && echo bk && vm_stat && echo bk &&  osascript -e 'path to frontmost application as text' ; }
+LOGFILE="$HOME/Documents/snap.log"
+function snap () { echo "%CPU %MEM USER PID COMMAND" ; ps aux | awk '{print $3,$4,$1,$2,$11}'| sort -rnk1,1 | grep -v '0.0 0.0' && echo '' && vm_stat && echo '' &&  osascript -e 'path to frontmost application as text' ; }
 
 
 #echo $(cat ~/Documents/snap.log | grep bk | wc -l) \/ 3 | bc
-while true; do echo bk >> ~/Documents/snap.log && date >> ~/Documents/snap.log && echo bk >> ~/Documents/snap.log && snap >> ~/Documents/snap.log && echo $(osascript getItermName.scpt) >> ~/Documents/snap.log; sleep 360 && echo not ; done
+
+
+while true;
+do
+    TIME="$(perl -e 'print(time());')" 
+    echo ${TIME} >> ${LOGFILE} && date >> ${LOGFILE} && echo ${TIME} >> ${LOGFILE} && snap >> ${LOGFILE} && echo $(osascript getItermName.scpt) >> ${LOGFILE}; 
+    sleep 360
+done
 
 
 
